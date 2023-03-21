67b45b3cd8bf071bdf57787a183dd0cde92db654
Lucas <122393305+HandyPancake@users.noreply.github.com>
2023-03-20 17:29:17 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,8 +1,72 @@
-//empty function; code will be added later
-function startButtonClick(){}
+function playSound(soundSrc){
+    var sound = document.createElement("audio");
+    sound.src = soundSrc;
+    sound.setAttribute("controls", "auto");
+    document.body.appendChild(sound);
+    play = function(){
+        sound.play();
+    }
+    stop = function(){
+        sound.pause();
+    }
+}
+
+
+var arrInterval = new Array();
+
 
 //empty function; code will be added later
-function stopButtonClick(){}
+function startButtonClick(){
+    //don't let user click the start button while the countodown is running
+    document.getElementById("btnStart").disabled = true;
+    document.getElementById("btnStop").disabled = false;
+
+    var countdownElem = document.getElementById("Countdown");
+    runTimer(countdownElem);
+}
+
+//empty function; code will be added later
+function stopButtonClick(){
+    document.getElementById("btnStart").disabled = false;
+    document.getElementById("btnStop").disabled = true;
+
+    for (counter = 0; counter < 11; counter++){
+        clearTimeout(arrInterval[counter]);
+    }
+
+}
+
+//This will ask for a first name, last name and badge number
+//The names need to be less than 20 characters and the badge number needs to be 3 characters or less
+function getUserInput(){
+    var fullName = ""
+    var badgeNumber = 0;
+
+    do{
+        var firstName = prompt("Please enter first name (under 10 characters): ");
+        var lastName = prompt("Please enter last name: (under 10 characters)");
+
+        fullName = firstName + " " + lastName;
+
+        if (fullName.length > 20){
+            alert("Please enter a shorter name. Lenght was: " + fullName.length);
+        }
+    }
+    while(fullName.length > 20);
+
+    do{
+        badgeNumber = prompt("Please enter your badge number (3 digits max): ");
+
+        if(badgeNumber > 999){
+            alert("Please enter a badge number with 3 digits or fewer.");
+        }
+    }
+    while(badgeNumber > 999);
+
+
+    return fullName + " " + badgeNumber;
+}
+
 
 
 function runTimer(countdownElem){
@@ -14,15 +78,15 @@ function runTimer(countdownElem){
 
  //how much time between actions
  var timeoutIncrement = 5000;
-
+//This is the thing that makes the loop
  for(var counter=0; counter < 11; counter++){
- setTimeout(function(){
+    arrInterval[counter] = setTimeout(function(){
     if (currTime == 0){
         alert("EXPLOSION!")
         countdownElem.innerHTML = "You Are Dead!!"
     }
-
-    else if (currTime <25){
+//this is the thing that makes the alternative message when under 25
+    else if (currTime < 25){
         countdownElem.innerHTML = "Warning Less than ½ way to launch, time left = " + currTime;
 
     }
